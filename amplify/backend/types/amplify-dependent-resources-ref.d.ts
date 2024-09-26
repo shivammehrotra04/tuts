85e0aa5f273257a0abec0b9b2c3c4cebaa81e8c7
shivammehrotra04 <shivammehrotra04@gmail.com>
2024-09-26 22:26:41 +0530

    add dependency of amplifyer and coginito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mytuts1appca3e93ba": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hey!! I`m shivam
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Authenticator>
+        {(data) => {
+          console.log(data);
+          return (
+            <main>
+              <header className="App-header">
+                <button
+                  onClick={data.signOut}
+                  style={{
+                    margin: "20px",
+                    fontSize: "0.8rem",
+                    padding: "5px 10px",
+                    marginTop: "20px",
+                  }}
+                >
+                  Sign Out
+                </button>
+              </header>
+            </main>
+          );
+        }}
+      </Authenticator>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
